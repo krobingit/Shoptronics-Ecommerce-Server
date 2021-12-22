4308611aa7e8dc4d3e0088a2ac3d7185656ae7de
krobingit <liorobin5@gmail.com>
2021-12-22 14:25:59 +0530

    changes

--- helper/razorpayment.js
+++ helper/razorpayment.js
@@ -7,10 +7,9 @@ var razpay = new Razorpay({
 });
 export const razorpay = async (req, res) => {
 
-  const { amount } = req.body;
  try {
   const response=await razpay.orders.create({
-   amount,
+   amount:"5000",
    currency: "INR",
    receipt: shortid.generate(),
   })
