036f3d80201f5c0729a452c868c29384e0b11d9f
krobingit <liorobin5@gmail.com>
2021-12-23 22:08:44 +0530

    change

--- helper/razorpayment.js
+++ helper/razorpayment.js
@@ -22,22 +22,31 @@ console.log(err)
   }
 
 }
-export const verification = async (req, res) => {
-  console.log(req.body)
-  await razpay.payments.fetch(req.body.razorpay_payment_id).then((pay) => {
-    if (pay.status === "captured") {
-      console.log(pay)
-      res.send({
-        message: "Payment Success"
-      })
-    }
-      else
-      return false;
+export const verification =  (req, res) => {
 
-  }
-)
+  /* razpay.payments.fetch(req.body.razorpay_payment_id)
+    .then((pay) => { console.log(pay) })
+*/
+// do a validation
+  const secret = 'leomessi';
 
+	console.log(req.body)
 
+	const crypto = require('crypto')
+
+	const shasum = crypto.createHmac('sha256', secret)
+	shasum.update(JSON.stringify(req.body))
+	const digest = shasum.digest('hex')
+
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		console.log('request is legit')
+		// process it
+		require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+	} else {
+		// pass it
+	}
+	res.json({ status: 'ok' })
 
 }
-
