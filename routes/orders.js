1613be608f4e35a8dce4590d34c9e07a3c50fa2f
krobingit <liorobin5@gmail.com>
2021-12-24 23:40:30 +0530

    ch

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { AuthorizeUser } from "../helper/authorization.middleware.js";
+//import { AuthorizeUser } from "../helper/authorization.middleware.js";
 const router = express.Router();
 import { helper } from "../helper/helper.orders.js";
 
 router
  .route("/")
- .post(AuthorizeUser,helper.postOrder)
+ .post(helper.postOrder)
 
 export const ordersRoute = router;
