744fda29ce8cc58f804b1fbad6792400949c6db5
krobingit <liorobin5@gmail.com>
2021-12-25 02:46:14 +0530

    orders

--- helper/helper.orders.js
+++ helper/helper.orders.js
@@ -1,12 +1,32 @@
-import { mongo } from "../mongo/mongo.js"
+import { mongo } from "../mongo/mongo.js";
 
 const helper={
-
+//create a new order
  async postOrder(req,res) {
+  try {
+   const order = { ...req.body, createdAt: new Date() }
+   const newOrder = await mongo.orders.insertOne(order);
+   res.status(201).send(newOrder);
+  }
+  catch (err)
+  {
+   console.log("Error creating new order", err);
+  res.status(500).send("Error occured")
+  }
+ },
+ //specific user's orders
+ async getOrder(req, res) {
+  try {
 
-  const order = { ...req.body, createdAt: new Date() }
-  const newOrder = await mongo.orders.insertOne(order);
-  res.status(201).send(newOrder);
+   const order = await mongo.orders.find({ userId: req.params.userid }).toArray();
+   res.status(200).send(order)
+  }
+  catch (err)
+  {
+   console.log("Error getting order", err);
+  res.status(500).send("Error occured")
+  }
+
  }
 
 }
--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { verifyToken } from "../helper/authorization.middleware.js";
+import { AuthorizeUser, verifyToken } from "../helper/authorization.middleware.js";
 const router = express.Router();
 import { helper } from "../helper/helper.orders.js";
 
 router
  .route("/")
- .post(verifyToken,helper.postOrder)
+ .post(verifyToken, helper.postOrder)
+
+router
+ .route("/:userid")
+.get(AuthorizeUser,helper.getOrder)
 
 export const ordersRoute = router;
