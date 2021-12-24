2790eb051c1c18423a6fb887fc755c19d62559c2
krobingit <liorobin5@gmail.com>
2021-12-25 00:16:13 +0530

    verifytoken

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,10 @@
 import express from "express";
-//import { AuthorizeUser } from "../helper/authorization.middleware.js";
+import { verifyToken } from "../helper/authorization.middleware.js";
 const router = express.Router();
 import { helper } from "../helper/helper.orders.js";
 
 router
  .route("/")
- .post(helper.postOrder)
+ .post(verifyToken,helper.postOrder)
 
 export const ordersRoute = router;
