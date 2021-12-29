30cc609c3aa86f07651771cc4df16c604ea2fd2b
krobingit <liorobin5@gmail.com>
2021-12-29 18:47:40 +0530

    chnage

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,13 @@ import { AuthorizeAdmin } from "../helper/authorization.middleware.js";
 import { helper } from "../helper/helper.products.js";
 const router = express.Router();
 
-//list all products
+//list all products/get and create product
 router
  .route("/")
  .get(helper.getAllProducts)
  .post(AuthorizeAdmin, helper.CreateProduct);
 
-//list specific product
+//list specific product/crud operations
 router
  .route("/:id")
  .get(helper.getProductById)
